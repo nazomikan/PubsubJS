881c7866329dd64b571b824b57c449c3cc266fd4
nazomikan <nazomikan@gmail.com>
2013-01-18 00:47:39 +0900

    more cleanup

--- pubsub.js
+++ pubsub.js
@@ -76,7 +76,7 @@
     }
 
     if (arguments.length > 1) {
-        throw new Error('Object.create implementation only accepts the first parameter.');
+      throw new Error('Object.create implementation only accepts the first parameter.');
     }
     function F() {}
     F.prototype = obj;
